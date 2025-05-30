77b26696552ed46a2336adec1b59b7b60275bc89
yuvraj042003 <yuvraj042003@gmail.com>
2025-05-31 05:14:21 +0530

    Initilize my project and setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div className='text-[20px] text-center text-blue-500 font-bold'>
+        <h1>Welcome to My React App</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
